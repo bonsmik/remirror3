6dc061d4a764a5527721719394b6806991360e88
Mikael von Bonsdorff <mikael.bonsdorff@monad.fi>
2024-03-22 17:36:36 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Editor from './Editor'
+
+function App() {
+
+  return (
+    <>
+      <h1>Test</h1>
+      <div><Editor/></div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Editor.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { BoldExtension } from 'remirror/extensions';
+
+import { Remirror, ThemeProvider, useRemirror } from '@remirror/react';
+import { ToggleBoldButton, Toolbar } from '@remirror/react-ui';
+
+const extensions = () => [new BoldExtension()];
+
+const ToggleBold = (): JSX.Element => {
+  const { manager, state, onChange } = useRemirror({
+    extensions: extensions,
+    content: '<p>Text in <b>bold</b></p>',
+    stringHandler: 'html',
+  });
+
+  return (
+    <ThemeProvider>
+      <Remirror
+        manager={manager}
+        autoFocus
+        onChange={onChange}
+        initialContent={state}
+        autoRender='end'
+      >
+        <Toolbar>
+          <ToggleBoldButton />
+        </Toolbar>
+      </Remirror>
+    </ThemeProvider>
+  );
+};
+
+export default ToggleBold;
